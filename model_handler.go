bb238c84c550ca8491ba88e9836f3ee5f9059a01
DaniyarGilimov <daniyargilimov@gmail.com>
2025-08-19 13:55:32 +0500

    only neccessary interface functions

--- model_handler.go
+++ model_handler.go
@@ -30,18 +30,19 @@ type Game interface {
 }
 
 type Telegram interface {
-	Create(chatId int64) error                      //
-	Delete(chatId int64) error                      //
-	BroadcastLSOF() error                           //
-	BroadcastSocketStartup() error                  //
+	Create(chatId int64) error     //
+	Delete(chatId int64) error     //
+	BroadcastLSOF() error          //
+	BroadcastSocketStartup() error //
+
 	BroadcastMessage(message string) error          //
 	SendMessage(chatId int64, message string) error //
-	GetMessage(messageType string) (string, error)  //
-	RegisterCount(chatId int64) error               //
-	ClickCount(chatId int64) error                  //
-	OpenedCount(chatId int64) error                 //
-	SendMessageDelete(message string) error         //
-	Doc(chatId int64) error                         //
+
+	GetMessage(messageType string) (string, error) //
+
+	WriteLSOF()
+
+	Doc(chatId int64) error //
 }
 
 // RoomInfo is room's information, that is static
