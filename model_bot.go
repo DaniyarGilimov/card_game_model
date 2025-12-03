d8bd6b85333b6553a93a3a189046a1bd51cc4b2a
DaniyarGilimov <daniyargilimov@gmail.com>
2025-12-03 17:00:18 +0500

    context of a bot

--- model_bot.go
+++ model_bot.go
@@ -6,6 +6,7 @@ type BotAI interface {
 	Initialize(player *Player, roomInfo *RoomInfo, gameActionCh func(raw []byte, ctx context.Context) bool)
 	ReceiveGameUpdate(message []byte, withSleep bool)
 	Shutdown()
+	Context() context.Context
 }
 
 type BotFactory func() BotAI
